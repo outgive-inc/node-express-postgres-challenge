d05460070e74ef4c1f6ef28cf48e788b5f3e9f7e
MarkusManas <markusmanas@gmail.com>
2020-11-25 17:36:22 +0800

    initial backend functionalities. initial queries, routes, and CRUD. TODO: error handling and validations

--- server/src/index.js
+++ server/src/index.js
@@ -30,7 +30,13 @@ pgClient.on('error', () => console.log('Lost Postgres connection'));
 pgClient
   .query(
     `
-     // TODO: Inser create table SQL query here
+    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+    CREATE TABLE IF NOT EXISTS tasks (
+      id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+      title TEXT NOT NULL,
+      details TEXT,
+      completed BOOLEAN DEFAULT FALSE
+    );
     `
   )
   .catch((err) => console.log(err));
@@ -39,28 +45,71 @@ pgClient
 
 // Get all to do list tasks
 app.get('/api/v1/tasks', async (req, res) => {
-  // TODO: Insert your route logic here
+  pgClient
+    .query("SELECT * FROM tasks;")
+    .then((result) => {
+      res.status(200).json(result.rows);
+    })
+    .catch((err) => console.log(err)
+  );
 });
 
 // Get a single todo task
-app.get('/api/v1/tasks', async (req, res) => {
-  // TODO: Insert your route logic here
+app.get('/api/v1/tasks/:id', async (req, res) => {
+  pgClient
+    .query(`SELECT * FROM tasks WHERE id='${req.params.id}'::uuid;`)
+    .then((result) => {
+      res.status(200).json(result.rows[0]);
+    })
+    .catch((err) => console.log(err)
+  );
 });
 
 // Create a todo task
 app.post('/api/v1/tasks', async (req, res) => {
-  // TODO: Insert your route logic here
+  pgClient
+    .query(
+      `
+      INSERT INTO tasks (id, title, details, completed)
+      VALUES(
+        '${uuid()}'::uuid, 
+        '${req.body.title}',
+        '${req.body.details}', 
+        ${req.body.completed}
+      );
+      `
+    )
+    .then((result) => {
+      res.status(200).send("success fully created task");
+    })
+    .catch((err) => console.log(err));
 });
 
 // Update a todo task
 app.put('/api/v1/tasks/:id', async (req, res) => {
-  // TODO: Insert your route logic here
+  pgClient
+    .query(`
+      UPDATE tasks  
+      SET title  = '${req.body.title}',
+          details = '${req.body.details}',
+          completed = ${req.body.completed}
+      WHERE id='${req.params.id}'::uuid;`)
+    .then((result) => {
+      res.status(200).send("Updated task.");
+    })
+    .catch((err) => console.log(err)
+  );
 });
 
 // Delete a todo task route
-
 app.delete('/api/v1/tasks/:id', async (req, res) => {
-  // TODO: Insert your route logic here
+  pgClient
+    .query(`DELETE FROM tasks WHERE id='${req.params.id}'::uuid;`)
+    .then((result) => {
+      res.status(200).send("Successfully deleted task.");
+    })
+    .catch((err) => console.log(err)
+  );
 });
 
 // Server
