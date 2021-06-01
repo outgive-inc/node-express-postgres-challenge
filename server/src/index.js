27f5ed765648f6dbbee72d5f315d7835b5e195fe
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-06-01 10:43:29 -0600

    added logger and routes

--- server/src/index.js
+++ server/src/index.js
@@ -1,69 +1,34 @@
-require('dotenv').config();
+require("dotenv").config();
 
 // Express App Setup
-const express = require('express');
-const http = require('http');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const uuid = require('uuid/v4');
+const express = require("express");
+const cors = require("cors");
+const logger = require("morgan");
 
-// Config
-const config = require('./config');
+const port = process.env.PORT || 3001;
+
+const pgClient = require("./models/db.config");
+
+const newTaskRouter = require("./routes/new");
+const allTasksRouter = require("./routes/index");
+const singleTaskRouter = require("./routes/show");
+const updateTaskRouter = require("./routes/update");
+const deleteTaskRouter = require("./routes/delete");
 
 // Initialization
 const app = express();
+
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(logger("dev"));
 
-// Postgres client
-const { Pool } = require('pg');
-const pgClient = new Pool({
-  user: config.pgUser,
-  host: config.pgHost,
-  database: config.pgDatabase,
-  password: config.pgPassword,
-  port: config.pgPort,
-});
-pgClient.on('error', () => console.log('Lost Postgres connection'));
+pgClient.on("error", () => console.log("Lost Postgres connection"));
 
-// TODO: Create initial DB table called task
-pgClient
-  .query(
-    `
-     // TODO: Inser create table SQL query here
-    `
-  )
-  .catch((err) => console.log(err));
-
-// Express route handlers
-
-// Get all to do list tasks
-app.get('/api/v1/tasks', async (req, res) => {
-  // TODO: Insert your route logic here
-});
-
-// Get a single todo task
-app.get('/api/v1/tasks', async (req, res) => {
-  // TODO: Insert your route logic here
-});
-
-// Create a todo task
-app.post('/api/v1/tasks', async (req, res) => {
-  // TODO: Insert your route logic here
-});
-
-// Update a todo task
-app.put('/api/v1/tasks/:id', async (req, res) => {
-  // TODO: Insert your route logic here
-});
-
-// Delete a todo task route
-
-app.delete('/api/v1/tasks/:id', async (req, res) => {
-  // TODO: Insert your route logic here
-});
+app.use(newTaskRouter);
+app.use(allTasksRouter);
+app.use(singleTaskRouter);
+app.use(updateTaskRouter);
+app.use(deleteTaskRouter);
 
 // Server
-const port = process.env.PORT || 3001;
-const server = http.createServer(app);
-server.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
