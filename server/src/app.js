ebe6acdb7cbf75226623a2d2dd9bceacee4d9f53
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-06-02 15:59:07 -0600

    typo

--- server/src/app.js
+++ server/src/app.js
@@ -24,7 +24,7 @@ app.use(updateTaskRouter);
 app.use(deleteTaskRouter);
 
 app.all("*", (req, res) => {
-  res.status(404).send({ msg: "Not found" });
+  res.status(404).send({ msg: "Route Not found" });
 });
 
 module.exports = app;
