fbfd8315adc8c0039484b0961e5ec1ee1e9e5e6a
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-06-02 15:57:34 -0600

    cleanup

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,8 @@
 require("dotenv").config();
-
 // Express App Setup
 const express = require("express");
 const cors = require("cors");
 const logger = require("morgan");
-
-const pgClient = require("./models/db.config");
 
 const newTaskRouter = require("./routes/new");
 const allTasksRouter = require("./routes/index");
@@ -19,8 +16,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(logger("dev"));
-
-// pgClient.on("error", () => console.log("Lost Postgres connection"));
 
 app.use(newTaskRouter);
 app.use(allTasksRouter);
