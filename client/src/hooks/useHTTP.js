113f80da6f102326a65616b4ccdd80966fc6cd37
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-06-02 14:30:54 -0600

    http hook

---
+++ client/src/hooks/useHTTP.js
@@ -0,0 +1,52 @@
+import { useCallback, useState } from "react";
+
+export const useHttp = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [message, setMessage] = useState("");
+
+  const request = useCallback(
+    async (url, method = "GET", body = null, headers = {}) => {
+      setLoading(true);
+      try {
+        if (body) {
+          body = JSON.stringify(body);
+          headers["Content-Type"] = "application/json";
+        }
+        const response = await fetch(url, { method, body, headers });
+        const data = await response.json();
+
+        if (!response.ok) {
+          throw new Error(data.message || "Something went wrong");
+        }
+
+        setLoading(false);
+
+        return data;
+      } catch (error) {
+        setLoading(false);
+        setError(error.message);
+        throw error;
+      }
+    },
+    []
+  );
+  const clearError = useCallback(
+    () => setTimeout(() => setError(null), 3000),
+    []
+  );
+  const clearMessage = useCallback(
+    () => setTimeout(() => setMessage(null), 3000),
+    []
+  );
+
+  return {
+    request,
+    loading,
+    error,
+    message,
+    clearError,
+    clearMessage,
+    setMessage,
+  };
+};
